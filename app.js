e57b23fc3467dc30daa6ca701ca3f05d0a2c04ab
reltaher <43248170+reltaher@users.noreply.github.com>
2021-07-02 22:50:43 -0400

    sticky footer/created a home page/made champions page its own directory

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 const NA1 = 'na1.api.riotgames.com';
 const championsData = 'http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json'
 
-app.get('/', async(req, res) => {
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/champions', async(req, res) => {
     // const response = await fetch(championsData);
     // const champions = await response.json();   
     // res.render('home', {champions});
@@ -21,14 +25,14 @@ app.get('/', async(req, res) => {
     await fetch(championsData).then(async response => {
         try {
             const champions = await response.json(); 
-            res.render('home', {champions});
+            res.render('champion/champions', {champions});
         } catch(error) {
             console.log(error);
         }
     })
 })
 
-app.get('/:champName', async(req, res) => {
+app.get('/champions/:champName', async(req, res) => {
     const championData = `http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions/${req.params.champName}.json`
     const response = await fetch(championData);
     const champion = await response.json(); 
